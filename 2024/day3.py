d72d6980d63170a95d135ec24998d68220be3e38
amaruy <yonzvi@gmail.com>
2024-12-02 22:45:25 -0800

    day3 regex

--- 2024/day3.py
+++ 2024/day3.py
@@ -1,30 +1,26 @@
-def solve():
-    text = r"""xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"""
+import re
+
+def solve(q=1):
     text = open('2024/data/input3.txt', 'r').read()
-    i = 0
-    n = len(text)
-    do = True
-    ans = 0
-    nums = '1234567890'
-    while i < len(text) - 7:
-        if do and text[i:i+4] == 'mul(':
-            if text[i+4] in nums:
-                j = i + 5
-                while j < n and text[j] in nums:
-                    j += 1
-                if j < n  and text[j] == ',':
-                    k = j + 1
-                    while k < n and text[k] in nums:
-                        k += 1
-                    if j + 1 < k < n and text[k] == ')':
-                        ans += int(text[i+4:j]) * int(text[j+1:k]) 
-        elif text[i] == 'd':
-            if text[i:i+7] == "don't()":
-                do = False
-            elif text[i:i+4] == 'do()':
-                do = True
-        i += 1
 
-    return ans  
+    muls = [(m.start(), 'mul', int(m.group(1)) * int(m.group(2))) 
+            for m in re.finditer(r'mul\((\d+),(\d+)\)', text)]
+    
+    controls = [(m.start(), 'control', m.group(1)) 
+                for m in re.finditer(r"(don't|do)\(\)", text)]
+    
+    operations = sorted(muls + controls)
+    
+    total = 0
+    active = True
+    
+    for position, op_type, value in operations:
+        if op_type == 'mul' and active:
+            total += value
+        elif q == 2 and op_type == 'control':
+            active = (value == 'do')
 
-print(solve())
+    return total
+
+print(solve(1))
+print(solve(2))
