80d42435fda1135bf531509ed9a3847851fee0ac
amaruy <yonzvi@gmail.com>
2024-12-11 13:56:54 -0800

    day 11 cleanup

--- 2024/day11.py
+++ 2024/day11.py
@@ -1,20 +1,10 @@
-text = open('2024/data/input11.txt', 'r').read()
 from functools import cache
 
-split = lambda x: (int(str(x)[:len(str(x)) // 2]), int(str(x)[len(str(x)) // 2:]))
-
 @cache
-def rec_count(stone, i):
+def r(n, i):
     if i == 0: return 1
-    if stone == 0: return rec_count(1, i - 1)
-    if len(str(stone)) % 2 == 0: x1, x2 = split(stone); return rec_count(x1, i - 1) + rec_count(x2, i - 1)
-    
-    return rec_count(stone * 2024, i -1)
+    if n == 0: return r(1, i - 1)
+    if len(s := str(n)) % 2 == 0: return r(int(s[:len(s) // 2]), i - 1) + r(int(s[len(s) // 2:]), i - 1)
+    return r(n * 2024, i - 1)
 
-count = 0
-for stone in map(int, text.split(' ')):
-    for i in range(74): rec_count(stone, i)
-    count += rec_count(stone, 75)
-
-print(count)
-    
+print(sum(r(int(n), 75) for n in open('2024/data/input11.txt').read().split()))
