bce1e2f38dab5ff100db7e0d52d400be32158879
amaruy <yonzvi@gmail.com>
2025-01-08 16:25:30 +0200

    day15 part2 took forever

--- 2024/day15.py
+++ 2024/day15.py
@@ -1,39 +1,14 @@
-text = """##########
-#..O..O.O#
-#......O.#
-#.OO..O.O#
-#..O@..O.#
-#O#..O...#
-#O..O..O.#
-#.OO.O.OO#
-#....O...#
-##########
+from collections import defaultdict
+from functools import cache
 
-<vv>^<v^>v>^vv^v>v<>v^v<v<^vv<<<^><<><>>v<vvv<>^v^>^<<<><<v<<<v^vv^v>^
-vvv<<^>^v^^><<>>><>^<<><^vv^^<>vvv<>><^^v>^>vv<>v<<<<v<^v>^<^^>>>^<v<v
-><>vv>v^v^<>><>>>><^^>vv>v<^^^>>v^v^<^^>v^^>v^<^v>v<>>v^v^<v>v^^<^^vv<
-<<v<^>>^^^^>>>v^<>vvv^><v<<<>^^^vv^<vvv>^>v<^^^^v<>^>vvvv><>>v^<<^^^^^
-^><^><>>><>^^<<^^v>>><^<v>^<vv>>v>>>^v><>^v><<<<v>>v<v<v>vvv>^<><<>^><
-^>><>^v<><^vvv<^^<><v<<<<<><^v<<<><<<^^<v<^^^><^>>^<v^><<<^>>^v<v^v<v^
->^>>^v>vv>^<<^v<>><<><<v<<v><>v<^vv<<<>^^v^>^^>>><<^v>>v^v><^^>>^<>vv^
-<><^^>^^^<><vvvvv^v<v<<>^v<v>v<<^><<><<><<<^^<<<^<<>><<><^^^>^^<>^>v<>
-^^>vv<^v^v<vv>^<><v<^v>^^^>>>^^vvv^>vvv<>>>^<^>>>>>^<<^v>^vvv<>^<><<v>
-v^^>>><<^^<>>^v^<v^vv<>v^<<>^<^v^v><^<<<><<^<v><v<>vv>>v><v^<vv<>v^<<^"""
-
-#text = open('2024/data/input15.txt', 'r').read()
+text = open('2024/data/input15.txt', 'r').read()
 
 grid, moves = text.split('\n\n')
 directions = {'>':(0, 1), 'v':(1, 0), '<':(0, -1), '^':(-1, 0)}
 grid = grid.splitlines()
 moves = moves.replace('\n', '')
 
-"""
-If the tile is #, the new map contains ## instead.
-If the tile is O, the new map contains [] instead.
-If the tile is ., the new map contains .. instead.
-If the tile is @, the new map contains @. instead.
-"""
-from collections import defaultdict
+
 pos = defaultdict(set)
 for i in range(len(grid)):
     for j in range(len(grid[0])):
@@ -68,116 +43,65 @@ for m in moves:
 f = lambda x: x[0] * 100 + x[1]
 print(sum(map(f, list(pos['O']))))
 
+
 """
 Q2
 """
-grid, moves = text.split('\n\n')
-directions = {'>':(0, 1), 'v':(1, 0), '<':(0, -1), '^':(-1, 0)}
-grid = grid.splitlines()
-moves = moves.replace('\n', '')
 
-"""
-If the tile is #, the new map contains ## instead.
-If the tile is O, the new map contains [] instead.
-If the tile is ., the new map contains .. instead.
-If the tile is @, the new map contains @. instead.
-"""
 extension = {'#':'##', '.':'..', '@':'@.', 'O':'[]'}
 grid = [list("".join(map(lambda x: extension[x], row))) for row in grid]
 
-
-from collections import defaultdict
-pos = defaultdict(set)
 for i in range(len(grid)):
     for j in range(len(grid[0])):
         if grid[i][j] == '@':
-            start = (i, j)
+            x, y = (i, j)
             break
 
+
+
+@cache
 def check(x, y, m):
-    dx, dy = m
+    dx, dy = directions[m]
+    if grid[x][y] == '@': return check(x+dx, y+dy, m)
     if grid[x][y] == '#': return False
     if grid[x][y] == '.': return True
 
     if m in '<>': return check(x+dx, y+dy, m)
 
     side = -1 if grid[x][y] == ']' else 1
-    return check(x+dx, y+dy, m) and check(x+side, y+side, m)
+    return check(x+dx, y+dy, m) and check(x+dx, y+side+dy, m)
 
 
-
-def move(x, y, m, current):
-    dx, dy = m
-    if grid[x][y] == '.':
-        grid[x][y] = current
-        return
-    
-    next_val = grid[x][y]
-    grid[x][y] = current
-
-    if m in '<>':
-        move(x+dx, y+dy, m, next_val)
-        return 
-    
-    side = -1 if grid[x][y] == ']' else 1
-    move(x+dx, y+dy, m, next_val)
-    
-    if grid[x][y] == ']':
-         return check
-
-    side = 1 if grid[x][y] == '[' else -1
-    if move(x+dx, y+dy, m, grid[x][y]) and move(x, y+side, m, grid[x][y+side]):
-        grid[x+dx][y+dy] = grid[x][y]
-        grid[x][y] = last
-        return True
-        
-        
+def move(x, y, m, moved, carry='.'):
+    if grid[x][y] != '.': 
+        dx, dy = directions[m]
+        moved.add((x, y))
+        move(x+dx, y+dy, m, moved, carry=grid[x][y])
+        if m in '^v':
+            side = -1 if grid[x][y] == ']' else 1
+            if grid[x][y] != '@' and (x, y+side) not in moved:
+                    moved.add((x, y+side))
+                    move(x, y+side, m, moved, carry='.')
+            if carry == '@':
+                grid[x][y+side] = '.'
 
 
+    grid[x][y] = carry
 
 for m in moves:
-    x, y = pos[robot]
-    dx, dy = directions[m]
-    if (x+dx, y+dy) in pos['#']:
-        continue
-    elif (x+dx, y+dy) in pos['.']:
-        pos[robot] = (x+dx, y+dy)
-        pos['.'].remove((x+dx, y+dy))
-        pos['.'].add((x, y))
-    else:
-        if m == '>':
-            while (x+dx, y+dy) in pos['[']: 
-                    x, y = x+dx, y+dy
-                    x, y = x+dx, y+dy
-            if (x+dx, y+dy) in pos['#']:
-                continue
-            elif (x+dx, y+dy) in pos['.']:
-                pos['.'].remove((x+dx, y+dy))
-                pos[']'].add((x+dx, y+dy))
-                pos['['].add((x, y))
-                x, y = pos[robot]
-                pos['['].remove((x+dx, y+dy))
-                pos[']'].remove((x + 2*dx, y + 2*dy))
-                pos[robot] = (x+dx, y+dy)
-                pos['.'].add((x, y))
-        elif m == '<':
-            while (x+dx, y+dy) in pos[']']: 
-                    x, y = x+dx, y+dy
-                    x, y = x+dx, y+dy
-            if (x+dx, y+dy) in pos['#']:
-                continue
-            elif (x+dx, y+dy) in pos['.']:
-                pos['.'].remove((x+dx, y+dy))
-                pos['['].add((x+dx, y+dy))
-                pos[']'].add((x, y))
-                x, y = pos[robot]
-                pos[']'].remove((x+dx, y+dy))
-                pos['['].remove((x + 2*dx, y + 2*dy))
-                pos[robot] = (x+dx, y+dy)
-                pos['.'].add((x, y)) 
-  
+    check.cache_clear()
+    if check(x, y, m):
+        move(x, y, m, set())
+        dx, dy = directions[m]
+        x, y = x+dx, y+dy
 
 
+pos = defaultdict(set)
+for i in range(len(grid)):
+    for j in range(len(grid[0])):
+        pos[grid[i][j]].add((i, j))
 f = lambda x: x[0] * 100 + x[1]
-print(sum(map(f, list(pos['O']))))
+print(sum(map(f, list(pos['[']))))
+         
+    
 
