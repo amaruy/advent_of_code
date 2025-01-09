50ced385c5097e9b577924d22ba96d8ec46bb7c0
amaruy <yonzvi@gmail.com>
2025-01-09 18:54:27 +0200

    networkx day23

--- 2024/day23.py
+++ 2024/day23.py
@@ -1,53 +1,9 @@
+import networkx as nx
 
-text = open('2024/data/input23.txt').read()
-e = [row.split('-') for row in text.splitlines()]
+G = nx.Graph([l.split('-') for l in open('2024/data/input23.txt').read().splitlines()])
 
-from collections import defaultdict
+# count triplets
+print(sum(any(n.startswith('t') for n in tr) for tr in nx.enumerate_all_cliques(G) if len(tr) == 3))
 
-edges = defaultdict(set)
-
-for node1, node2 in e:
-    edges[node1].add(node2)
-    edges[node2].add(node1)
-
-
-triplets = set()
-for node1, neighbors in edges.items():
-    for node2 in neighbors:
-        for node in edges[node1] & edges[node2]:
-            triplets.add(tuple(sorted([node1, node2, node])))
-
-tc = 0
-for t in triplets:
-    for n in t:
-        if n.startswith('t'):
-            tc += 1
-            break
-        
-
-print(tc)
-
-def bron_kerbosch(graph, R, P, X, cliques):
-    if not P and not X:
-        # Found a maximal clique
-        cliques.append(R)
-        return
-    # Choose a pivot to reduce recursive calls
-    pivot = max(P | X, key=lambda node: len(graph[node]))
-    for v in P - graph[pivot]:
-        bron_kerbosch(
-            graph,
-            R | {v},
-            P & graph[v],
-            X & graph[v],
-            cliques
-        )
-        P.remove(v)
-        X.add(v)
-
-cliques = []
-bron_kerbosch(edges, set(), set(edges.keys()), set(), cliques)
-
-max_clique = max(cliques, key=len) 
-
-print(",".join(sorted(max_clique)))
+# Find max clique
+print(','.join(sorted(max(nx.find_cliques(G), key=len))))
